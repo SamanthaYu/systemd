3379ab32ae02222e8b091b1d9a66f6524646a1b9
SamanthaYu <samanthayu36@gmail.com>
2020-04-29 13:26:21 -0700

    Update compilation database to use test_cache.c

--- src/async-csa/test_cache.c
+++ src/async-csa/test_cache.c
@@ -1,25 +1,24 @@
 #include "hashmap.h"
 #include "machine-image.h"
 
-static void test_clear_hashmap() {
+static void test_clear_hashmap(void) {
     Hashmap* cache = hashmap_new(NULL);
 
-    Image* image = NULL;
-    int r = image_from_path("/", &image);
-    if (r < 0) {
-        return r;
-    }
+    Image* image1 = NULL;
+    Image* image2 = NULL;
+    Image* image3 = NULL;
+    image_from_path("/", &image1);
+    image_from_path("/", &image2);
+    image_from_path("/", &image3);
 
-    r = hashmap_put(cache, image->name, image);
-    if (r < 0) {
-        return r;
-    }
+    hashmap_put(cache, "Image1", image1);
+    hashmap_put(cache, "Image2", image2);
+    hashmap_put(cache, "Image3", image3);
 
     hashmap_clear(cache);
 
     // Checker should detect use-after-free here
-    Image* image2 = image;
-    free(image2);
+    printf("Image %s\n", image1->name);
 }
 
 int main(int argc, char *argv[]) {
